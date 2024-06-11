313013a1f8ab76f520c923eb3aa5911f5c8e519f
daniel <u202121440@upc.edu.pe>
2024-06-10 20:51:24 -0500

    feat: update package.json, add test-map

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
 import DeliveryRouteComponent from "@/delivery/delivery-route.component.vue";
+import TestMapComponent from "@/delivery/test-map.component.vue";
 
 const router=createRouter({
     history: createWebHistory(),
     routes:[
-        {path:'/', component:DeliveryRouteComponent}
+        {path:'/', component:TestMapComponent}
     ]
 })
 
--- src/services/delivery-api.service.js
+++ src/services/delivery-api.service.js
@@ -7,23 +7,4 @@ export class DeliveryApiService {
     async getLocals(){
         return await http.get('/locals');
     }
-    /*async getStates() {
-        return await http.get('/states');
-    }
-    async getCities() {
-        return await http.get('/cities');
-    }
-    async getCitiesByState(stateId) {
-        return await http.get(`/cities?state=${stateId}`);
-    }
-    async getRestaurants() {
-        return await http.get('/restaurants');
-    }
-    async getRestaurantByCity(cityId) {
-        return await http.get(`/restaurants?city=${cityId}`)
-    }
-    async createRoute(body) {
-        return await http.post('/routes',body)
-    }*/
-
 }
